4afca79a892fb5ba5ae3e61a3817e21a16be52e3
Antoine Galice <antoine.galice@seagate.com>
2020-09-02 17:13:43 +0200

    Options for host app: burst type + set size

--- samples/subsys/usb/hid_issue_sample/host_application/hid-issue.c
+++ samples/subsys/usb/hid_issue_sample/host_application/hid-issue.c
@@ -31,7 +31,7 @@
 
 #include <signal.h>
 
-#define SEND_REPORT_SIZE 128
+#define SEND_REPORT_SIZE 4096
 #define RECV_REPORT_SIZE 4096 /* Need to be large enough */
 
 static volatile int fd = -1;
@@ -47,16 +47,52 @@ void int_handler(int val)
 	exit(0);
 }
 
+void usage()
+{
+	printf("Usage: ./hid-issue <hidraw path> <type> <Set size>\n"
+	       "hidraw path: /dev/hidrawX\n"
+	       "type:\n"
+	       "\t'get' to burst only get report requests \n"
+	       "\t      (will ignore 'Set size' arg, put any value)\n"
+	       "\t'set' to burst only set report requests\n"
+	       "\t'alt' to alternate set and get report requests\n"
+	       "Set size: Set report size (max %d)\n",
+	       SEND_REPORT_SIZE);
+}
+
 int main(int argc, char **argv)
 {
 	const uint8_t send_report[SEND_REPORT_SIZE + 1] = {0};
 	uint8_t recv_report[RECV_REPORT_SIZE] = {0};
-	
-	if (argc != 2) {
-		printf("Please specify device hidraw path (/dev/hidrawX).\n");
+	enum {SET, GET, ALT};
+	char type;
+	int set_size = -1;
+
+	if (argc != 4) {
+		usage();
+		return 1;
+	}
+
+	if (strncmp(argv[2], "get", 4) == 0) {
+		type = GET;
+	} else if (strncmp(argv[2], "set", 4) == 0) {
+		type = SET;
+	} else if (strncmp(argv[2], "alt", 4) == 0) {
+		type = ALT;
+	} else {
+		usage();
 		return 1;
 	}
 	
+	if (type == SET || type == ALT) {
+		set_size = strtol(argv[3], NULL, 10);
+		if (set_size < 1 || set_size > SEND_REPORT_SIZE) {
+			printf("Set size is incorrect\n");
+			usage();
+			return 1;
+		}
+	}
+
 	fd = open(argv[1], O_RDWR);
 	if (fd < 0) {
 		perror("Unable to open device");
@@ -65,20 +101,27 @@ int main(int argc, char **argv)
 
 	printf("%s: Start set/get report. Set size: %d\n"
 	       "use [ctrl-c] to stop the program and print the result.\n",
-	       __func__, SEND_REPORT_SIZE);
+	       __func__, set_size);
 	signal(SIGINT, int_handler);
 	while (1) {
 		//printf("Count = %u\n", count);
-		if (ioctl(fd, HIDIOCSFEATURE(sizeof(send_report)), send_report) < 0) {
-			perror("Send feature report");
-			error_count++;
+		if (type == SET || type == ALT ) {
+			if (ioctl(fd, HIDIOCSFEATURE(set_size + 1),
+				  send_report) < 0) {
+				perror("Set feature report");
+				error_count++;
+			}
+			count++;
 		}
-		if (ioctl(fd, HIDIOCGFEATURE(sizeof(recv_report)), recv_report) < 0) {
-			perror("Receive feature report");
-			error_count++;
+		if (type == GET || type == ALT) {
+			recv_report[0] = 0;
+			if (ioctl(fd, HIDIOCGFEATURE(sizeof(recv_report)),
+				  recv_report) < 0) {
+				perror("Get feature report");
+				error_count++;
+			}
+			count++;
 		}
-		recv_report[0] = 0;
-		count += 2;
  	}
 
 	close(fd);
